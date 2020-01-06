b4341c0bb59c3699555e03925cae6868e8c22ab2
pankajrana737 <pankajrana737@gmail.com>
2020-01-06 15:37:34 +0530

    new file:   .gitignore 	new file:   .mvn/wrapper/MavenWrapperDownloader.java 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/pankaj/rana/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/pankaj/rana/microservice/currencyconversionservice/CurrencyConversionServiceApplicationTests.java

---
+++ src/main/java/com/pankaj/rana/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pankaj.rana.microservice.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
