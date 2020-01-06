db8ff9d766a381f36c1ef3d8483d80f6184337e8
pankajrana737 <pankajrana737@gmail.com>
2020-01-06 17:44:41 +0530

    modified:   pom.xml 	modified:   src/main/java/com/pankaj/rana/microservice/currencyconversionservice/bean/CurrencyExchangeServiceProxy.java

--- src/main/java/com/pankaj/rana/microservice/currencyconversionservice/bean/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/pankaj/rana/microservice/currencyconversionservice/bean/CurrencyExchangeServiceProxy.java
@@ -1,5 +1,6 @@
 package com.pankaj.rana.microservice.currencyconversionservice.bean;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 //name = service name Spring.application.name
 //url of service
 //feign is rest service client
-@FeignClient(name = "currency-exchange-service",url = "localhost:8000")
+//hard code url to resolve this use ribbon load balancer
+// comment this @FeignClient(name = "currency-exchange-service",url = "localhost:8000")
+// adding @RibbonClient(name = "-service name")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service",url = "localhost:8000")
 public interface CurrencyExchangeServiceProxy {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
  public	CurrencyConversionBean reteriveExchangeValue(@PathVariable("from") String from, @ PathVariable("to") String to) ;
