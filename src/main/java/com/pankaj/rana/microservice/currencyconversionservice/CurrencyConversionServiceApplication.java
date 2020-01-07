3828b3e84d33ae5ba72caaad9fca70a78cddc53b
pankajrana737 <pankajrana737@gmail.com>
2020-01-07 13:10:03 +0530

    adding comment

--- src/main/java/com/pankaj/rana/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/pankaj/rana/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,13 @@ package com.pankaj.rana.microservice.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //enable feign scan packages
 @EnableFeignClients(value = {"com.pankaj.rana.microservice.currencyconversionservice"})
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
